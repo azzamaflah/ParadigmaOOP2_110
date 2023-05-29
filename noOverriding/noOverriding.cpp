01e20b5e350153c41deaf24c64fb2283e75db299
azzamaflah <azzamhebat46@gmail.com>
2023-05-29 13:09:06 +0700

    class derived public baseClass

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 class baseClass {
 public:
-	virtual void perkenalan() final {
+	virtual void perkenalan() { //final
 		cout << "Hallo saya Function dari base class";
 	}
 };
+
+class derivedClass : public baseClass { 
+public:
+	void perkenalan() final {
+		cout << "Hallo saya function dari derived class";
+	}
+};
+
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
